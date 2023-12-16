8e78c7eec6e409244af12e5aa75e64d27b36fd98
Farhan <farhanshokat@gmail.com>
2023-12-16 16:16:32 +0500

    Fonts Configuration

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,6 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="h-screen">
+      <h1 className="h1-bold">This is my heading</h1>
       <UserButton afterSignOutUrl="/" />
     </div>
   );
