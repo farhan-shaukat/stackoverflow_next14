98f3d58dad73c695fa3235448f4e16c3851babfb
Farhan <farhanshokat@gmail.com>
2023-12-26 21:54:01 +0500

    Home Filters

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,8 @@
+import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
 import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 import React from "react";
 
@@ -25,8 +27,14 @@ const Home = () => {
           otherClasses="flex-1"
         />
 
-        <Filter />
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
+
+      <HomeFilters />
     </>
   );
 };
