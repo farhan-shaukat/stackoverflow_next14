361e12b3cb2d8c173ff351ee3a381b86114be862
Farhan <farhanshokat@gmail.com>
2024-01-03 23:40:52 +0500

    Added Clert Webhook API route to public.

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,7 +7,7 @@ import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
@@ -52,6 +52,8 @@ export async function POST(req: Request) {
   }
 
   const eventType = evt.type;
+
+  console.log(eventType);
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
