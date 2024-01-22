b4cefd853b8b8001fce303def33e07a3e7a74c0d
Farhan <farhanshokat@gmail.com>
2024-01-22 23:53:01 +0500

    Question Details

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import "../styles/prism.css"
 import React from "react";
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
